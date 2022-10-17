8f428f369dea6797e3ce0555a94ddb36451b8d55
247Suave <101532464+247Suave@users.noreply.github.com>
2022-10-17 15:45:36 +0100

    Code

---
+++ Week 2/10 - Simple For Loops/Task 10.4.py
@@ -0,0 +1,13 @@
+print("Which strange markings do you see?.")
+Markings = input()
+print("\n")
+
+print("Identifying...")
+print("\n")
+
+for i in range(0, len(Markings), 1):
+
+        print("index",i, ":", Markings[i])
+
+print("\n")
+print("Done!")
